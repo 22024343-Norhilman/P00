c5d13014bd7e37a9361291e831cbd6ac05336759
22024343 <22024343@10.175.4.108>
2023-05-26 11:38:58 +0800

    first push to P00 repository in GitHbu

---
+++ P00/src/Calculator.java
@@ -0,0 +1,22 @@
+
+public class Calculator {
+	public int add(int a, int b) {
+		return a + b;
+
+	}
+
+	public int subtract(int a, int b) {
+		return a - b;
+	}
+
+	public int multiple(int a, int b) {
+		return a * b;
+	}
+
+	public int divide(int a, int b) {
+		if(b == 0) {
+			System.out.println("You cannot divide by 0");
+		}
+		return a / b;
+	}
+}
